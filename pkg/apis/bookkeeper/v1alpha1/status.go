ef2af722605efcc7e7e05ffb8c71cca481c620fc
SrishT <Srishti.Thakkar@dell.com>
2020-04-23 17:29:27 +0530

    Issue 6: Publish events for metadata cleanup failure
    Signed-off-by: SrishT <Srishti.Thakkar@dell.com>

--- pkg/apis/bookkeeper/v1alpha1/status.go
+++ pkg/apis/bookkeeper/v1alpha1/status.go
@@ -26,9 +26,9 @@ const (
 	ClusterConditionError                          = "Error"
 
 	// Reasons for cluster upgrading condition
-	UpdatingBookkeeperReason   = "Updating Bookkeeper"
-	UpgradeErrorReason         = "Upgrade Error"
-	RollbackErrorReason        = "Rollback Error"
+	UpdatingBookkeeperReason = "Updating Bookkeeper"
+	UpgradeErrorReason       = "Upgrade Error"
+	RollbackErrorReason      = "Rollback Error"
 )
 
 // BookkeeperClusterStatus defines the observed state of BookkeeperCluster
