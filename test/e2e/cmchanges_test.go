9e01ae6d985a8dc99f958349b0d994b2383c8cf5
anishakj <anisha.kj@dell.com>
2020-11-11 07:03:06 -0800

    added end to end test
    Signed-off-by: anishakj <anisha.kj@dell.com>

---
+++ test/e2e/cmchanges_test.go
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package e2e
+
+import (
+	"strings"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	bookkeeper_e2eutil "github.com/pravega/bookkeeper-operator/pkg/test/e2e/e2eutil"
+)
+
+func testCMUpgradeCluster(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	doCleanup := true
+	ctx := framework.NewTestCtx(t)
+	defer func() {
+		if doCleanup {
+			ctx.Cleanup()
+		}
+	}()
+
+	namespace, err := ctx.GetNamespace()
+	g.Expect(err).NotTo(HaveOccurred())
+	f := framework.Global
+
+	cluster := bookkeeper_e2eutil.NewDefaultCluster(namespace)
+
+	cluster.WithDefaults()
+
+	cluster.Spec.Options["minorCompactionThreshold"] = "0.4"
+
+	bookkeeper, err := bookkeeper_e2eutil.CreateBKCluster(t, f, ctx, cluster)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	// A default bookkeeper cluster should have 2 pods:  1 controller, 1 segment store
+	podSize := 3
+	err = bookkeeper_e2eutil.WaitForBookkeeperClusterToBecomeReady(t, f, ctx, bookkeeper, podSize)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	// This is to get the latest bookkeeper cluster object
+	bookkeeper, err = bookkeeper_e2eutil.GetBKCluster(t, f, ctx, bookkeeper)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	//updating bookkeeper option
+	bookkeeper.Spec.Options["minorCompactionThreshold"] = "0.5"
+
+	//updating bookkeepercluster
+	err = bookkeeper_e2eutil.UpdateBKCluster(t, f, ctx, bookkeeper)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	//checking if the upgrade of options was successful
+	err = bookkeeper_e2eutil.WaitForCMBKClusterToUpgrade(t, f, ctx, bookkeeper)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	// This is to get the latest bookkeeper cluster object
+	bookkeeper, err = bookkeeper_e2eutil.GetBKCluster(t, f, ctx, bookkeeper)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	//updating bookkeeper option
+	bookkeeper.Spec.Options["journalDirectories"] = "journal"
+
+	//updating bookkeepercluster
+	err = bookkeeper_e2eutil.UpdateBKCluster(t, f, ctx, bookkeeper)
+
+	//should give an error
+	g.Expect(strings.ContainsAny(err.Error(), "path of journal directories should not be changed")).To(Equal(true))
+
+	// Delete cluster
+	err = bookkeeper_e2eutil.DeleteBKCluster(t, f, ctx, bookkeeper)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	// No need to do cleanup since the cluster CR has already been deleted
+	doCleanup = false
+
+	err = bookkeeper_e2eutil.WaitForBKClusterToTerminate(t, f, ctx, bookkeeper)
+	g.Expect(err).NotTo(HaveOccurred())
+
+}
