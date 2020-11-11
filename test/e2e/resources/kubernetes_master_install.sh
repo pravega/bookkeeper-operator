479bd71fae1a0c29060ae34c21cfee0e538f7593
prabhaker24 <prabhaker.saxena@dell.com>
2020-11-11 11:22:23 +0530

    added back delete cluster
    Signed-off-by: prabhaker24 <prabhaker.saxena@dell.com>

--- test/e2e/resources/kubernetes_master_install.sh
+++ test/e2e/resources/kubernetes_master_install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+echo "TCPKeepAlive yes" >> /etc/ssh/sshd_config
+echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+echo "ClientAliveCountMax 3" >> /etc/ssh/sshd_config
+service sshd restart
 sudo apt-get update
 echo "update done"
 sudo apt install docker.io -y
@@ -11,19 +14,19 @@ sudo systemctl daemon-reload
 sudo systemctl start nfs-common
 sudo systemctl status nfs-common
 apt-get update && apt-get install -y \
-  apt-transport-https ca-certificates curl software-properties-common gnupg2 
+  apt-transport-https ca-certificates curl software-properties-common gnupg2
 echo "installed certs"
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
   | sudo tee -a /etc/apt/sources.list.d/kubernetes.list \
-  && sudo apt-get update 
+  && sudo apt-get update
 sudo apt-get update \
   && sudo apt-get install -yq \
   kubelet \
   kubeadm \
   kubernetes-cni
 sudo apt-mark hold kubelet kubeadm kubectl
-UUID=`cat /etc/fstab | grep swap | awk '{print $1}' | tr -d "#UUID="` 
+UUID=`cat /etc/fstab | grep swap | awk '{print $1}' | tr -d "#UUID="`
 sed -i '2 s/^/#/' /etc/fstab
 echo "swapoff UUID=$UUID"
 swapoff UUID=$UUID
@@ -34,6 +37,6 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl get nodes
 kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
-kubectl get nodes 
+kubectl get nodes
 sudo apt-get install binutils bison gcc -y
 mkdir /export
