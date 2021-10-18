3df8176f8450a5632260d7a6a9bd61c7a740df95
Aaron <aaron.wu@dell.com>
2021-10-18 15:53:30 +0800

    rephrase wording
    Signed-off-by: Aaron <aaron.wu@dell.com>

--- pkg/controller/config/config.go
+++ pkg/controller/config/config.go
@@ -18,6 +18,6 @@ var TestMode bool
 // DisableFinalizer disables the finalizers for bookkeeper clusters and
 // skips the znode cleanup phase when bookkeeper cluster get deleted.
 // This is useful when operator deletion may happen before bookkeeper clusters deletion.
-// NOTE: enabling this flag with caution! It causes stale znode data in zk uncleaned and
+// NOTE: enabling this flag with caution! It causes stale znode data in zk and
 // leads to conflicts with subsequent bookkeeper clusters deployments
 var DisableFinalizer bool
