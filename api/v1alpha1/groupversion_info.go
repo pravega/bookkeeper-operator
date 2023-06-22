8231430f6a74df089645575dfc50079d18fd3bd9
anisha.kj <anisha.kj@dell.com>
2023-06-21 21:44:02 -0700

    Fixed formatting issues
    Signed-off-by: anisha.kj <anisha.kj@dell.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the bookkeeper v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=bookkeeper.pravega.io
+// +kubebuilder:object:generate=true
+// +groupName=bookkeeper.pravega.io
 package v1alpha1
 
 import (
